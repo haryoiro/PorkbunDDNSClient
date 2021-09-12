9b7df1070a648e4d8a3611132f5fa1956c39aee5
haryoiro <mizusecocolte@gmail.com>
2021-09-12 13:42:27 +0900

    change dep

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.106.0/flags/mod.ts";
 import { cron } from "https://deno.land/x/deno_cron/cron.ts";
-
-export { cron, parse };
+import yargs from "https://cdn.deno.land/yargs/versions/yargs-v16.2.1-deno/raw/deno.ts";
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+export { _, cron, yargs };
